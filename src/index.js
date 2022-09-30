bd2aafbc2fc4a7160bc98142ceaee06094bcd3c6
WClifford1 <williamdavidclifford@gmail.com>
2022-09-30 16:34:40 +1000

    add post route

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+const port = 3000
+
+app.post('/asset', (req, res) => {
+  const fileName = req.headers["file-name"]
+  const contentType = req.headers["content-type"]
+
+  const localFileName = path.join(__dirname, "../uploads", fileName)
+
+  const fileWriteStream = fs.createWriteStream(localFileName)
+  req.pipe(fileWriteStream)
+    .on('error', err => {
+      console.error(`Error writing ${localFileName}`)
+      console.error(err)
+      res.sendStatus(500)
+    })
+    .on('finish', () => {
+      console.log(`Done writing ${localFileName}`)
+      res.sendStatus(200)
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
