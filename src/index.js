12c66641c6100dc8b5cd1150d6c30324349fdd00
WClifford1 <williamdavidclifford@gmail.com>
2022-10-02 14:03:30 +1000

    Refactored function to stream storage

--- src/index.js
+++ src/index.js
@@ -18,37 +18,28 @@ async function main() {
 
   const db = client.db(dbName)
   const assetCollections = db.collection('assets')
-  const assets = await assetCollections.find().toArray()
-  console.log('Initial Assets')
- console.log("assets called from index.js ", assets)
 
   // Start the REST API
   const app = express()
   const port = 3000
 
-  app.post('/asset', (req, res) => {
+  app.post('/asset', async (req, res) => {
+
+    const assetId = new ObjectId()
     const fileName = req.headers["file-name"]
     const contentType = req.headers["content-type"]
   
-    const localFileName = path.join(__dirname, "../uploads", fileName)
+    const localFileName = path.join(__dirname, "../uploads", assetId.toString())
   
-    const fileWriteStream = fs.createWriteStream(localFileName)
-    req.pipe(fileWriteStream)
-      .on('error', err => {
-        console.error(`Error writing ${localFileName}`)
-        console.error(err)
-        res.sendStatus(500)
-      })
-      .on('finish', () => {
-        console.log(`Done writing ${localFileName}`)
-        res.sendStatus(200)
-      })
+    await streamToStorage(localFileName, req)
+
+    res.sendStatus(200)
   })
   
   app.get('/asset', (req, res) => {
     const fileName = req.query.fileName
-  
     res.writeHead(200, {
+  
       "Content/Type": "image/png"
     })
   
@@ -68,3 +59,17 @@ main()
     console.error(err)
     process.exit(1)
   })
+
+// Streams an input stream to local file storage
+async function streamToStorage(localFileName, inputStream) {
+  return new Promise((resolve, reject) => {
+    const fileWriteStream = fs.createWriteStream(localFileName)
+    inputStream.pipe(fileWriteStream)
+      .on('error', err => {
+        reject(err)
+      })
+      .on('finish', () => {
+        resolve()
+      })
+  })
+}
